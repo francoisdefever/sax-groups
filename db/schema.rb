88debbfc13f8a0e793b67721a7f1267030dcf1e4
francoisdefever <fdefever.work@gmail.com>
2016-02-17 10:30:37 +0100

    rake db:migrate && setup

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160217090323) do
+ActiveRecord::Schema.define(version: 20160217092717) do
+
+  create_table "groups", force: :cascade do |t|
+    t.string   "name"
+    t.string   "room"
+    t.string   "task"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
