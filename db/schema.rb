8c298829e5618f6433b6b1f0c206f5943101f81f
francoisdefever <fdefever.work@gmail.com>
2016-02-19 12:00:24 +0100

    test rake db:setup to deploy

--- db/schema.rb
+++ db/schema.rb
@@ -13,12 +13,24 @@
 
 ActiveRecord::Schema.define(version: 20160217092717) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "groups", force: :cascade do |t|
     t.string   "name"
     t.string   "room"
     t.string   "task"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "user"
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -31,6 +43,6 @@ ActiveRecord::Schema.define(version: 20160217092717) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "users", ["group_id"], name: "index_users_on_group_id"
+  add_index "users", ["group_id"], name: "index_users_on_group_id", using: :btree
 
 end
